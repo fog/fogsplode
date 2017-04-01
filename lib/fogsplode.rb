92aaa3466d6e227b0a2b04bbd63e3ab8563a00a4
geemus <geemus@gmail.com>
2017-04-01 16:54:34 -0500

    add tasks require

--- lib/fogsplode.rb
+++ lib/fogsplode.rb
@@ -4,6 +4,7 @@ require File.join(dir, 'init')
 require File.join(dir, 'gemfiles')
 require File.join(dir, 'open_source')
 require File.join(dir, 'readme')
+require File.join(dir, 'tasks')
 require File.join(dir, 'travis')
 require File.join(dir, 'version')
 
